875bc50ef0c40035b3c4f5eff611dc0a98e200eb
Tim Kotowski <tkotowski7@gmail.com>
2020-08-30 09:13:41 -0500

    first push

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+	package main
+
+
+	import (
+		"fmt"
+		"net/http"
+	)
+
+	func main() {
+		links := []string {
+			"https://www.youtube.com/",
+			"https://www.twitch.tv/",
+			"https://golang.org/",
+			"http://timkotowski.com/",
+		}
+
+		c := make(chan string)
+
+		for _, link := range links {
+			go checkLink(link, c)
+		}
+
+		for  l := range c {
+			go checkLink(l, c)
+		}
+	}
+
+		func checkLink(link string, c chan string) {
+
+				_, err := http.Get(link)
+				if err != nil {
+					fmt.Println(link, "might be down!")
+					c <- link
+					return
+				}
+				fmt.Println(link, "is up!")
+				c <- link
+		}
